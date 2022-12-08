5d25ed0bc673335aa285deefb8b4eb3ea132a688
Huseyin Aykis <h.aykis25@gmail.com>
2022-12-08 23:05:49 +0100

    ert

--- src/assignment/Question1.java
+++ src/assignment/Question1.java
@@ -1,5 +1,40 @@
 package assignment;
 
 public class Question1 {
+public static void main (String   [] args){
+		
+		byte variable1=18;
+		short a=-32768;
+		int b=127;
+		long c=9223372036854775807L;
+		float d=2.569f;
+		double e=-128.256;
+		boolean f=true;
+		char g='X';
+		char h=65;
+		boolean i=9<=10;
+		
+		System.out.println("variable degerimiz= "+variable1);
+		System.out.println("Byte nin max degeri= "+Byte.MAX_VALUE);
+		System.out.println("Byte in min degeri"+Byte.MIN_VALUE);
+		System.out.println("a nin degeri= "+a);
+		System.out.println("short in max degeri= "+Short.MAX_VALUE);
+		System.out.println("short un min degeri= "+Short.MIN_VALUE);
+		System.out.println("b nin degeri= "+b);
+		System.out.println("int b nin max degeri= "+Integer.MAX_VALUE);
+		System.out.println("long degerinin max degeri"+Long.MAX_VALUE);
+		System.out.println("c nin degeri= "+c);
+		System.out.println("float degeri= "+d);
+		System.out.println("double degeri= "+e);
+		System.out.println("double max degeri= "+Double.MAX_VALUE);
+		System.out.println("boolean degeri= "+f);
+		System.out.println("char g nin degeri= "+g);
+		System.out.println("h degeri= "+h);
+		System.out.println("i nin degeri= "+i);
+		
+		
+		
+		
+	}
 
 }
